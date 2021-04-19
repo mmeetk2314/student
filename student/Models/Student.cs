b980b398b70221b89208bb69e193d8f2b4774078
mmeetk2314 <71989905+mmeetk2314@users.noreply.github.com>
2021-04-19 13:54:59 +1200

    Add comments in the models folder in the student assesement.

--- student/Models/Student.cs
+++ student/Models/Student.cs
@@ -9,11 +9,17 @@ namespace student.Models
     public class Student
     {
         [Key]
+        //This is the primary key
         public int Id { get; set; }
+         //This is the variable to store the student name
         public string student_Name { get; set; }
+         //This is the variable to store the student email
         public string student_Email { get; set; }
+         //This is the variable to store the student mobile
         public string student_Mobile { get; set; }
+         //This is the variable to store the course name
         public string course_Name { get; set; }
+         //This is the variable to store the course duration
         public string course_Duration { get; set; }
     }
 }
